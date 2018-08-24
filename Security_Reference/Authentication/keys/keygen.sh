7083c8ad64c8f730762370f3a3da4925cf4357ca
Tknott95 <tknott95@hotmail.com>
2018-08-24 07:06:19 -0600

    RSA

--- Security_Reference/Authentication/keys/keygen.sh
+++ Security_Reference/Authentication/keys/keygen.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-openssl genrsa -out flui.key 2048
-openssl req -new -x509 -key flui.key -out flui.pem -days 3650
+openssl genrsa -out tk.key 2048
+openssl req -new -x509 -key tk.key -out tk.pem -days 3650
+openssl genrsa -out tk.rsa 1024
+openssl rsa -in tk.rsa -pubout > tk.rsa.pub
+
+
